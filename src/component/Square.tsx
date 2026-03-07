de7f4d6189165f3949a5eabcadaacec002dcc6a2
Khushansh <khushanshbaliyan@gmail.com>
2026-03-07 23:56:36 +0530

    updated boardArray for better functionality of game engine

--- src/component/Square.tsx
+++ src/component/Square.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-type props ={
-    column: string,
-    cindex:number,
-    rindex:number
+import Peice from './Peice'
+type props = {
+  column: string,
+  cindex: number,
+  rindex: number
 }
 
-const Square = ({column, cindex, rindex}: props) => {
+const Square = ({ column, cindex, rindex }: props) => {
+   
   return (
-   <div key={cindex} className={`flex items-center justify-center w-full h-full text-5xl ${(rindex + cindex) % 2 === 0 ? 'bg-white text-black' : 'bg-black text-white'}`}>
-              {column}
-            </div>
+    <div key={cindex} className={`flex items-center justify-center w-full h-full text-5xl ${(rindex + cindex) % 2 === 0 ? 'bg-white text-black' : 'bg-black text-white'}`}>
+          {column}
+    {/* <Peice/> */}
+    </div>
   )
 }
 
--- src/engine/InitialBoard.tsx
+++ src/engine/InitialBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import Board from '../component/Board'
 type props = {
   initBoard: (name: string[][]) => void,
@@ -6,16 +6,14 @@ type props = {
 }
 const InitialBoard = ({ initBoard, board }: props) => {
   const boardArray: string[][] = [
-    ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
-    ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
+    ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
+    ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
-    ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
-    ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
-
-
+    ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+    ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr']
   ]
 
   useEffect(() => {
